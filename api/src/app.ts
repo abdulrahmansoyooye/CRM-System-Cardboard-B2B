6b1c54e62ab59c5680d7642c475adb516482053f
abdulrahmansoyooye <abdulrahmansoyooye@gmail.com>
2026-07-05 21:59:05 +0100

    new commit

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
 import MongoStore from 'rate-limit-mongo';
-import xss from 'xss-clean';
 import mongoose from 'mongoose';
 import globalErrorHandler from './middleware/error.middleware';
 import morganMiddleware from './middleware/morgan.middleware';
@@ -46,7 +45,6 @@ app.use(helmet());
 app.use(corsOptions ? cors(corsOptions) : cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(xss());
 
 // Serve uploaded files statically
 app.use('/uploads', express.static('uploads'));
--- api/src/types/xss-clean.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'xss-clean' {
-  import { Request, Response, NextFunction } from 'express';
-
-  function xssClean(): (req: Request, res: Response, next: NextFunction) => void;
-
-  export default xssClean;
-}
